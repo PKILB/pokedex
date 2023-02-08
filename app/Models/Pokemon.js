89475c52eb4bbfbdfb8e0d629a08cbd40746470c
Payton Boone <paytonboone30@gmail.com>
2023-02-08 15:50:09 -0700

    template created

---
+++ app/Models/Pokemon.js
@@ -0,0 +1,14 @@
+
+
+export class WildPokemon {
+    constructor(data) {
+        this.name = data.name
+        this.url = data.url
+    }
+
+    static PokemonListTemplate(pokemon) {
+        return  /*html*/`
+        
+        `
+    }
+}
