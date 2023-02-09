e386db1a22494518fc4d4bbbe1ee2e4672d8ce30
PKILB <paytonboone30@gmail.com>
2023-02-08 20:32:30 -0700

    really close

--- app/Models/Pokemon.js
+++ app/Models/Pokemon.js
@@ -3,34 +3,35 @@
 export class Pokemon {
     constructor(data) {
         this.name = data.name
-        this.img = data.img
+        this.img = data.other.dream_world.front_default
         this.weight = data.weight
         this.height = data.height
         this.user = data.user
+        this.type = data.type
     }
 
 get BigActivePokemonTemplate() {
-    return /*html*/`
+    return `
     <div class="col-10 m-auto">
     <div class="row m-auto p-4 bg-light rounded elevation-2 border border-danger border-2 text-center">
       <div>
-        <h1 class="fs-3">Bulbasaur</h1>
+        <h1 class="fs-3">${this.name}</h1>
       </div>
     </div>
     <div class="row m-auto my-5 p-4 text-center">
       <div>
-        <img height="140" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur">
+        ${this.img}
       </div>
     </div>
     <div class="row m-auto my-5 p-4 bg-light rounded elevation-2 border border-danger border-2 text-center">
       <span class="col-6 text-start">
-        <h1 class="fs-5">Height: </h1>
+        <h1 class="fs-5">Height: ${this.height} </h1>
       </span>
       <span class="col-6 text-end">
-        <h1 class="fs-5">Weight: </h1>
+        <h1 class="fs-5">Weight: ${this.weight} </h1>
       </span>
       <span class="text-start">
-        <h1 class="fs-5">Type: </h1>
+        <h1 class="fs-5">Type: ${this.type}</h1>
       </span>
       <div class="text-end">
         <button class="btn btn-outline-danger">Catch!</button>
